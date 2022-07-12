1d21e548c8f2e8c1606a92a06e5db0f98517dcaa
Mark <angel9899@mail.ru>
2022-07-12 16:01:19 +0300

    Menu opening bug fix

--- pages/hotelinfo.php
+++ pages/hotelinfo.php
@@ -40,8 +40,24 @@
 									if (isset($_GET['page'])) {
 										$page = $_GET['page'];
 									}
-									include_once "menu.php";
 							?>
+									<ul class="nav nav-tabs nav-justified">
+									  <li <? echo ($page == 1) ? "class='active'" : ""?>>
+									    <a href="../index.php?page=1">Tours</a>
+									  </li>
+									  <li <? echo ($page == 2) ? "class='active'" : ""?>>
+									    <a href="../index.php?page=2">Comments</a>
+									  </li>
+									  <li <? echo ($page == 3) ? "class='active'" : ""?>>
+									    <a href="../index.php?page=3">Registration</a>
+									  </li>
+									  <li <? echo ($page == 4) ? "class='active'" : ""?>>
+									    <a href="../index.php?page=4">Admin Forms</a>
+									  </li>
+									  <li <? echo ($page == 5) ? "class='active'" : ""?>>
+									    <a href="../index.php?page=5">Privat</a>
+									  </li>
+									</ul>
 					</nav>
 			</div>
 			<nav class="header-nav">
